3006b256645b0ee84503fd939a2862505dc9a4e0
Fethi Güralp Albayram <fethi.grlp@gmail.com>
2024-08-05 16:28:57 +0300

    fix

---
+++ AgriculturePresentation/ViewComponents/_DashboardChartPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _DashboardChartPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var value = 452000;
+            ViewBag.totalSales = value;
+            return View();
+        }
+    }
+}
---
+++ AgriculturePresentation/ViewComponents/_DashboardOverviewPartial.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class _DashboardOverviewPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            AgricultureContext context = new AgricultureContext();
+
+            ViewBag.teamCount = context.Teams.Count();
+            ViewBag.serviceCount = context.Services.Count();
+            ViewBag.messageCount = context.Contacts.Count();
+
+            ViewBag.announcementTrue = context.Announcements.Where(x => x.Status == true).Count();
+            ViewBag.announcementFalse = context.Announcements.Where(x => x.Status == false).Count();
+
+            return View();  
+        }
+    }
+}
